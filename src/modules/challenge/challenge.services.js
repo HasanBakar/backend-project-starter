ed6dba12c69611f6677bc94d37bb9e2c08ee4941
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2024-06-09 00:46:54 +0600

    :tada: initially project setup and rud locally

--- src/modules/challenge/challenge.services.js
+++ src/modules/challenge/challenge.services.js
@@ -3,6 +3,7 @@ import MChallenge from "./challenge.model.js";
 const getAllChallengesFromDB = async () => {
   try {
     const result = await MChallenge.find();
+
     return result;
   } catch (error) {
     console.log(error);
